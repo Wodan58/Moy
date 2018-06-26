c9975054323e6ea23328e7323de38a73c1ddd4db
Wodan58 <r.wiersma26@kpnplanet.nl>
2018-06-26 10:56:32 +0200

    parse.sh

--- parse.sh
+++ parse.sh
@@ -2,11 +2,11 @@
 #  Generate parse.h
 #
 PATH=.:usr/local/wbin:$PATH
-if [ parse.y -nt parse.h ]
-then
+# if [ parse.y -nt parse.h ]
+# then
 echo Building parse.h
 echo $*
 $*
-else
-echo parse.h is up-to-date
-fi
+# else
+# echo parse.h is up-to-date
+# fi
