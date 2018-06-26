026b4c4475e521683491243c34dad3996f5711d7
Wodan58 <r.wiersma26@kpnplanet.nl>
2018-06-26 13:10:31 +0200

    parse.sh

--- parse.sh
+++ parse.sh
@@ -5,11 +5,11 @@ PATH=.:usr/local/wbin:$PATH
 if [ ! -f parse.h ]
 then
 echo creating parse.h
-$*
+bison -doparse.c parse.y
 elif [ parse.y -nt parse.h ]
 then
 echo updating parse.h
-$*
+bison -doparse.c parse.y
 else
 echo parse.h is up-to-date
 fi
