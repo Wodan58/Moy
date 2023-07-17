1fd239d411c52fa902f35776f0e79ef7f834a051
Wodan58 <r.wiersma26@gmail.com>
2023-07-17 13:00:24 +0200

    Tests

--- src/gmtime.c
+++ src/gmtime.c
@@ -1,17 +1,10 @@
 /*
     module  : gmtime.c
-    version : 1.1
-    date    : 07/10/23
+    version : 1.2
+    date    : 07/17/23
 */
 #ifndef GMTIME_C
 #define GMTIME_C
-
-#ifdef WIN32
-void gmtime_r(time_t *t, struct tm *tm)
-{
-    *tm = *gmtime(t);
-}
-#endif
 
 /**
 OK 1710  gmtime  :  DA	I  ->  T
@@ -23,35 +16,35 @@ isdst is false; weekday is 1 = Monday ... 7 = Sunday.
 void gmtime_(pEnv env)
 {
     static int daynums[] = { 7, 1, 2, 3, 4, 5, 6 };
-    struct tm t;
+    struct tm *t;
     time_t timval;
     Node node, temp;
 
     PARM(1, UNMKTIME);
     node = vec_pop(env->stck);
     timval = node.u.num;
-    gmtime_r(&timval, &t);
+    t = gmtime(&timval);
     vec_init(temp.u.lis);
-    node.u.num = daynums[t.tm_wday];
+    node.u.num = daynums[t->tm_wday];
     node.op = INTEGER_;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_yday;
+    node.u.num = t->tm_yday;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_isdst;
+    node.u.num = t->tm_isdst;
     node.op = BOOLEAN_;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_sec;
+    node.u.num = t->tm_sec;
     node.op = INTEGER_;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_min;
+    node.u.num = t->tm_min;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_hour;
+    node.u.num = t->tm_hour;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_mday;
+    node.u.num = t->tm_mday;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_mon + 1;
+    node.u.num = t->tm_mon + 1;
     vec_push(temp.u.lis, node);
-    node.u.num = t.tm_year + 1900;
+    node.u.num = t->tm_year + 1900;
     vec_push(temp.u.lis, node);
     temp.op = LIST_;
     vec_push(env->stck, temp);
